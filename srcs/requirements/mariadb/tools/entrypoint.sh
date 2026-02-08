65d285626314f90a11fb066fe1337b7b75b9261b
pmachado <silva.mmmpedro@gmail.com>
2026-02-08 21:51:21 +0000

    Configure MariaDB container with entrypoint initialization

---
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# This script is the entry point for the MariaDB container.
+
+#exit immediately if any command exits with a non-zero status.
+set -e
+
+#set a marker file to indicate that the database has been initialized
+INIT_FILE="/var/lib/mysql/.initialized"
+
+#check if the marker file exists, if not, initialize the database
+if [ ! -f "$INIT_FILE" ]; then
+	echo "mariadb initialization ..."
+	mariadbd &
+	#wait until mariadb is ready to accept connections
+	until mariadb-admin ping --silent; do
+		sleep 1
+	done
+
+	echo "mariadb initialized successfully. Initializing SQL..."
+
+	#run the SQL initialization script to set up the database schema and initial data
+	    mariadb << EOF
+		CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
+		CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+		GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
+		ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+EOF
+
+	#Create the marker file to indicate that the database has been initialized
+	touch "$INIT_FILE"
+	mariadb-admin shutdown
+	echo "mariadb initialization finished."
+fi
+
+#execute the command passed as arguments to the script
+exec "$@"
