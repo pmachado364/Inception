0048f41fb0c5771a96e4fb94bc73830583bb69a7
pmachado <silva.mmmpedro@gmail.com>
2026-02-20 19:14:00 +0000

    wordpress: refactor php-fpm config and finalize service setup
    - introduced custom www.conf
    - removed sed lines from Dockerfile
    - finalized entrypoint script logic
    - integrated wordpress service into docker-compose

--- srcs/requirements/wordpress/tools/entrypoint_wp.sh
+++ srcs/requirements/wordpress/tools/entrypoint_wp.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
 
-# This script is the entry point for the WordPress container.
-
-#exit immediately if any command exits with a non-zero status.
 set -e
 
-#verify is the database is ready to accept connections
-until mariadb-admin ping --silent; do
-	sleep 1
+echo "Waiting on MariaDB to be ready."
+
+#wait until the database is ready
+until mysqladmin ping \
+	-h mariadb \
+	-u "$DB_USER" \
+	-p"$DB_PASSWORD" \
+	--silent; do
+	echo "MariaDB is not ready yet. Retrying in 5 seconds."
+	sleep 5
 done
 
-#check if the WordPress configuration file exists, if not, create it
+echo "MariaDB is ready."
+
+#if the wordpress config file doesn't exist, create it and download wordpress
 if [ ! -f "/var/www/html/wp-config.php" ]; then
-	echo "WordPress configuration initiating ..."
-	exec php-fpm -F
-	else
-	echo "WordPress configuration already exists, starting the server ..."
-	exec php-fpm -F
+	echo "WordPress is not configured. Configuring now."
+
+	#we need to download wordpress and create the config file before starting php-fpm, otherwise it will fail because of missing files
+	wp core download --allow-root
+	# create the wp-config.php file with the database credentials and the database host set to mariadb, which is the name of the service in docker-compose
+	wp config create \
+	--dbname="$DB_NAME" \
+	--dbuser="$DB_USER" \
+	--dbpass="$DB_PASSWORD" \
+	--dbhost="mariadb" \
+	--allow-root
+
+	#if the database is empty, we need to install wordpress
+	wp core install \
+	--url="localhost" \
+	--title="Inception WordPress" \
+	--admin_user="$WP_ADMIN_USER" \
+	--admin_password="$WP_ADMIN_PASSWORD" \
+	--admin_email="$WP_ADMIN_EMAIL" \
+	--allow-root
 fi
+
+chown -R www-data:www-data /var/www/html
+
+echo "WordPress is ready."
+
+exec php-fpm8.2 -F
